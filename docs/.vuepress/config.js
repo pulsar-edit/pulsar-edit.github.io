879a18eb8592736dff1b51d8f7ce9f3afb24a656
Josh <16845458+kaosine@users.noreply.github.com>
2022-09-19 13:53:45 -0400

    resolved conflicts

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,7 +15,34 @@ export default defineUserConfig({
     },
     navbar: navbar_en,
     displayAllHeaders: true,
-    sidebar: sidebar_en,
+    sidebar: [
+      {
+        title: 'Documentation Home',
+        text: 'Documentation Home',
+        link: '/docs/',
+        children: [
+          '/docs/user_guide/',
+          '/docs/packages/'
+        ]
+      },
+      {
+        title: 'Core Packages',
+        text: 'Core Packages',
+        link: '/docs/packages/core/',
+        initialOpenGroupIndex: 2,
+        children: [
+          '/docs/packages/core/atom-languageclient/',
+          '/docs/packages/core/autocomplete-plus/',
+          '/docs/packages/core/github/',
+          '/docs/packages/core/ide-java/'
+        ]
+      },
+      {
+        title: 'Community Packages',
+        text: 'Community Packages',
+        link: '/docs/packages/community'
+      }
+    ],
     repo: "https://github.com/pulsar-edit"
   })
 });
--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -2,34 +2,38 @@ const sidebar_en = {
   '/': [
 
   ],
-  [
+  '/docs/': [
     {
-      title: 'Documentation Home',
-      text: 'Documentation Home',
-      link: '/docs/',
-      children: [
-        '/docs/user_guide/',
-        '/docs/packages/'
+      [
+        {
+          title: 'Documentation Home',
+          text: 'Documentation Home',
+          link: '/docs/',
+          children: [
+            '/docs/user_guide/',
+            '/docs/packages/'
+          ]
+        },
+        {
+          title: 'Core Packages',
+          text: 'Core Packages',
+          link: '/docs/packages/core/',
+          initialOpenGroupIndex: '2',
+          children: [
+            '/docs/packages/core/atom-languageclient/',
+            '/docs/packages/core/autocomplete-plus/',
+            '/docs/packages/core/github/',
+            '/docs/packages/core/ide-java/'
+          ]
+        },
+        {
+          title: 'Community Packages',
+          text: 'Community Packages',
+          link: '/docs/packages/community'
+        }
       ]
-    },
-    {
-      title: 'Core Packages',
-      text: 'Core Packages',
-      link: '/docs/packages/core/',
-      initialOpenGroupIndex: 2,
-      children: [
-        '/docs/packages/core/atom-languageclient/',
-        '/docs/packages/core/autocomplete-plus/',
-        '/docs/packages/core/github/',
-        '/docs/packages/core/ide-java/'
-      ]
-    },
-    {
-      title: 'Community Packages',
-      text: 'Community Packages',
-      link: '/docs/packages/community'
     }
-  ],
+  ]
   '/about/': [
 
   ]
