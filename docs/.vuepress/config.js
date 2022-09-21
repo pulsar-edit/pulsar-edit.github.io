0189bc10264d5aa7730bffac3cb8dcf339a2d4c7
Josh <16845458+kaosine@users.noreply.github.com>
2022-09-21 19:09:06 -0400

    dang imports not working

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,8 +3,7 @@ import { getDirname, path } from '@vuepress/utils';
 import { navbar_en } from "./navbar.js";
 import { sidebar_en } from "./sidebar.js";
 
-
-const __dirname = getDirname(import.meta.url);
+const __dirname = getDirname(import.meta.url)
 
 export default defineUserConfig({
   lang: 'en-US',
@@ -12,7 +11,10 @@ export default defineUserConfig({
   description: 'Next generation hackable editor for the future',
   editLinks: true,
   alias: {
-    '@images': 'https:\/\/github.com\/pulsar-edit\/.github',
+    '@images': path.resolve(__dirname, 'pulsar-media/images'),
+  },
+  devServer: {
+    https: true
   },
   theme: defaultTheme({
     locales: {
